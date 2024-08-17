c0bebdfb30ae0b0f435a3dcc174649201ac1417e
Paritosh <79096636+paritosh142@users.noreply.github.com>
2024-08-17 15:42:33 +0530

    update

--- Email-Automator/app.py
+++ Email-Automator/app.py
@@ -5,12 +5,16 @@ import webbrowser
 import urllib.parse
 from dotenv import load_dotenv
 import os
+from flask_cors import CORS
 
 
 # Load environment variables from .env file
 load_dotenv()
 
+
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/')
 def index():
